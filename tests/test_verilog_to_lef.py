b8e2db2446e85c5e3427ed9aeb38ece0ab71c667
sehuang <sehuang@berkeley.edu>
2019-09-23 12:23:01 -0700

    Modified techfile path to be OS agnostic

--- tests/test_verilog_to_lef.py
+++ tests/test_verilog_to_lef.py
@@ -1,10 +1,14 @@
 from verilog_pin_extract import VerilogModule
 from verilog2lef import *
 import pprint
+import pathlib # I hate you Microsoft
 
 # test_mod = VerilogModule('Memory141', filename='Memory141.v', constfile='const.vh')
 test2_mod = VerilogModule('TestParamsAndPorts', filename='test_module.v')
-test_techfile = "/Users/sehuang/hammer/src/hammer-vlsi/technology/asap7/asap7.tech.json"
+projects_dir = pathlib.Path(__file__).parents[2]
+test_techfile = projects_dir / 'hammer/src/hammer-vlsi/technology/asap7/asap7.tech.json'
+test_techfile = str(test_techfile)
+# test_techfile = "../hammer/src/hammer-vlsi/technology/asap7/asap7.tech.json"
 
 def test_v2lef():
 	bbox = BBoxLEF(test2_mod, test_techfile)
