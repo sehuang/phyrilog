3b539c8e894dca6cdcbf5d235c8bda1f50177ff6
sehuang <sehuang@berkeley.edu>
2019-09-23 17:48:00 -0700

    Changed physical mapping dict schema

--- verilog2lef.py
+++ verilog2lef.py
@@ -9,9 +9,9 @@ class PHYObject:
 
 	def add_rect(self, layer, left_x, bot_y, right_x, top_y):
 		if layer in self.phys_map.keys():
-			self.phys_map[layer]['coord'].append = [left_x, bot_y, right_x, top_y]
+			self.phys_map[layer].append = [left_x, bot_y, right_x, top_y]
 		else:
-			self.phys_map[layer]['coord'] = [[left_x, bot_y, right_x, top_y]]
+			self.phys_map[layer] = [[left_x, bot_y, right_x, top_y]]
 
 	# self.phys_map[layer]['shape'] = 'RECT'
 
@@ -40,7 +40,7 @@ class PHYPortPin(PHYObject):
 		if layer in self.phys_map.keys():
 			self.phys_map[layer]['coord'].append = [left_x, bot_y, left_x + self.x_width, bot_y + self.y_width]
 		else:
-			self.phys_map[layer] = {'coord':[[left_x, bot_y, left_x + self.x_width, bot_y + self.y_width]]}
+			self.phys_map[layer] = [[left_x, bot_y, left_x + self.x_width, bot_y + self.y_width]]
 
 	def write_lef_block_struct(self, bus_idx=None):
 		direction_str = f"DIRECTION {self.direction} ;"
@@ -60,9 +60,9 @@ class PHYBBox(PHYObject):
 
 	def add_rect(self, layer, left_x, bot_y, right_x, top_y):
 		if layer in self.phys_map.keys():
-			self.phys_map[layer]['coord'].append = [left_x, bot_y, right_x, top_y]
+			self.phys_map[layer].append = [left_x, bot_y, right_x, top_y]
 		else:
-			self.phys_map[layer] = {'coord':[[left_x, bot_y, right_x, top_y]]}
+			self.phys_map[layer] = [[left_x, bot_y, right_x, top_y]]
 
 
 # self.phys_map[layer]['shape'] = 'RECT'
@@ -85,10 +85,13 @@ class PHYDesign:
 
 		self.spec_dict = spec_dict
 		self.pins = {}
-		self.defaults = {'input_side': 'left',
+		self.defaults = {'origin': [0,0],
+						 'input_side': 'left',
 						 'output_side': 'right',
 						 'aspect_ratio': [1, 1],
 						 'pin_margin': False,
+						 'symmetry': 'X Y',
+						 'site': 'core',
 						 'pins': {'h_layer': "M2",
 								  'v_layer': "M3"
 								  }
@@ -98,6 +101,7 @@ class PHYDesign:
 			self.specs.update(spec_dict)
 		self.x_width = self.specs.get('xwidth', None)
 		self.y_width = self.specs.get('ywidth', None)
+
 
 	def _extract_tech_json_info(self, techfile):
 		with open(techfile) as file:
@@ -175,12 +179,12 @@ class PHYDesign:
 				self.n_inputs += (direction == 'input') * (pin_info['bus_max'] + 1)
 				self.n_outputs += (direction == 'output') * (pin_info['bus_max'] + 1)
 				for pin in range(pin_info['bus_max'] + 1):
-					self.pins[pin_name + '_' + str(pin)] = PHYPortPin(pin_info, layer, side, x_width, y_width,
+					self.pins[pin_name + '_' + str(pin)] = PHYPortPin(pin_info, layer, side, round(x_width,3), round(y_width,3),
 																	  bus_idx=pin)
 			else:
 				self.n_inputs += direction == 'input'
 				self.n_outputs += direction == 'output'
-				self.pins[pin_name] = PHYPortPin(pin_info, layer, side, x_width, y_width)
+				self.pins[pin_name] = PHYPortPin(pin_info, layer, side, round(x_width,3), round(y_width,3))
 
 	def define_design_boundaries(self):
 		pass
@@ -249,14 +253,14 @@ class BBoxPHY(PHYDesign):
 		if self.specs['pin_margin']:
 			x_width = x_width + v_pin_pitch
 			y_width = y_width + h_pin_pitch
-		self.bbox_x_width = x_width
-		self.bbox_y_width = y_width
-		self.bbox_left_margin = max([pin.x_width for pin in  self.pin_sides_dict['left']], default=0)
-		self.bbox_right_margin = max([pin.x_width for pin in self.pin_sides_dict['right']], default=0)
-		self.bbox_top_margin = max([pin.y_width for pin in self.pin_sides_dict['top']], default=0)
-		self.bbox_bot_margin = max([pin.y_width for pin in self.pin_sides_dict['bottom']], default=0)
-		self.x_width = self.bbox_x_width + self.bbox_left_margin + self.bbox_right_margin
-		self.y_width = self.bbox_y_width + self.bbox_top_margin + self.bbox_bot_margin
+		self.bbox_x_width = round(x_width, 3)
+		self.bbox_y_width = round(y_width, 3)
+		self.bbox_left_margin = round(max([pin.x_width for pin in  self.pin_sides_dict['left']], default=0), 3)
+		self.bbox_right_margin = round(max([pin.x_width for pin in self.pin_sides_dict['right']], default=0), 3)
+		self.bbox_top_margin = round(max([pin.y_width for pin in self.pin_sides_dict['top']], default=0), 3)
+		self.bbox_bot_margin = round(max([pin.y_width for pin in self.pin_sides_dict['bottom']], default=0), 3)
+		self.x_width = round(self.bbox_x_width + self.bbox_left_margin + self.bbox_right_margin, 3)
+		self.y_width = round(self.bbox_y_width + self.bbox_top_margin + self.bbox_bot_margin, 3)
 
 	def build_design_repr(self):
 		origin = [0, 0]
@@ -272,26 +276,26 @@ class BBoxPHY(PHYDesign):
 		else:
 			x_corr = 0
 			y_corr = 0
-		left_pin_lower_corner = [bbox_bot_left_corner[0], round((self.bbox_bot_margin + y_corr) / h_pin_width)]
-		right_pin_lower_corner = [bbox_top_right_corner[0], round((self.bbox_bot_margin + y_corr) / h_pin_width)]
-		bot_pin_lower_corner =[round((bbox_bot_left_corner[0] + x_corr) / v_pin_width), 0]
-		top_pin_lower_corner =[round((bbox_top_right_corner[0] + x_corr) / v_pin_width), self.y_width]
+		left_pin_lower_corner = [bbox_bot_left_corner[0], round(round((self.bbox_bot_margin + y_corr) / h_pin_width) * h_pin_pitch, 3)]
+		right_pin_lower_corner = [bbox_top_right_corner[0], round(round((self.bbox_bot_margin + y_corr) / h_pin_width) * h_pin_pitch, 3)]
+		bot_pin_lower_corner =[round(round((bbox_bot_left_corner[0] + x_corr) / v_pin_width), 3), 0]
+		top_pin_lower_corner =[round(round((bbox_top_right_corner[0] + x_corr) / v_pin_width), 3), self.y_width]
 
 		self.bboxes = {
 			'BBOX': PHYBBox(self.metals.keys(), bbox_bot_left_corner[0], bbox_bot_left_corner[1], self.bbox_x_width,
 							self.bbox_y_width)}
 		for pin in self.pin_sides_dict['left']:
 			pin.add_rect(self.specs['pins']['h_layer'], left_pin_lower_corner[0], left_pin_lower_corner[1])
-			left_pin_lower_corner[1] = left_pin_lower_corner[1] + h_pin_width + h_pin_pitch
+			left_pin_lower_corner[1] = round(left_pin_lower_corner[1] + h_pin_width + h_pin_pitch, 3)
 
 		for pin in self.pin_sides_dict['right']:
 			pin.add_rect(self.specs['pins']['h_layer'], right_pin_lower_corner[0], right_pin_lower_corner[1])
-			right_pin_lower_corner[1] = right_pin_lower_corner[1] + h_pin_width + h_pin_pitch
+			right_pin_lower_corner[1] = round(right_pin_lower_corner[1] + h_pin_width + h_pin_pitch, 3)
 
 		for pin in self.pin_sides_dict['bottom']:
 			pin.add_rect(self.specs['pins']['v_layer'], bot_pin_lower_corner[0], bot_pin_lower_corner[1])
-			bot_pin_lower_corner[0] = bot_pin_lower_corner[0] + v_pin_width + v_pin_pitch
+			bot_pin_lower_corner[0] = round(bot_pin_lower_corner[0] + v_pin_width + v_pin_pitch, 3)
 
 		for pin in self.pin_sides_dict['top']:
 			pin.add_rect(self.specs['pins']['v_layer'], top_pin_lower_corner[0], top_pin_lower_corner[1])
-			top_pin_lower_corner[0] = top_pin_lower_corner[0] + v_pin_width + v_pin_pitch
+			top_pin_lower_corner[0] = round(top_pin_lower_corner[0] + v_pin_width + v_pin_pitch, 3)
