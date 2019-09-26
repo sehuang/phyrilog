f4582778eebf1630035c876eb1d6772fbe3630f2
sehuang <sehuang@berkeley.edu>
2019-09-26 01:22:20 -0700

    Memory141 test

--- tests/test_verilog_to_lef.py
+++ tests/test_verilog_to_lef.py
@@ -4,7 +4,7 @@ from LEFBuilder import *
 import pprint
 import pathlib  # I hate you Microsoft
 
-# test_mod = VerilogModule('Memory141', filename='Memory141.v', constfile='const.vh')
+test_mod = VerilogModule('Memory141', filename='Memory141.v', constfile='const.vh')
 test2_mod = VerilogModule('TestParamsAndPorts', filename='test_module.v')
 projects_dir = pathlib.Path(__file__).parents[2]
 test_techfile = projects_dir / 'hammer/src/hammer-vlsi/technology/asap7/asap7.tech.json'
@@ -27,6 +27,11 @@ def test_v2lef(spec_dict):
 	bbox.build_design_repr()
 	return bbox
 
+def test_v1lef(spec_dict):
+	bbox = BBoxPHY(test_mod, test_techfile, spec_dict=spec_dict)
+	bbox_lef = BBoxLEFBuilder(bbox, filename='Memory141.lef', indent_char_width=2)
+	pprint.pprint(bbox_lef.blocks)
+	bbox_lef.write_lef()
 
 if __name__ == '__main__':
 	spec_dict = {'pin_margin': True,
@@ -36,10 +41,13 @@ if __name__ == '__main__':
 					 'v_layer': 'M5'
 				 },
 				 'exclude_layers': ['Pad']}
-	bbox = test_v2lef(spec_dict)
-	bbox_lef = BBoxLEFBuilder()
-	bbox_lef.make_lef_dict(bbox)
-	print(bbox.pins['sum_0'].phys_map)
-	pprint.pprint(bbox_lef.blocks)
-	pprint.pprint(bbox_lef.blocks[test2_mod.name].blocks[''].type)
-
+	test_v1lef(spec_dict)
+	# bbox = test_v2lef(spec_dict)
+	# bbox_lef = BBoxLEFBuilder(filename='test_lef.lef', indent_char_width = 2)
+	# bbox_lef.make_lef_dict(bbox)
+	# print(bbox.pins['sum[0]'].phys_map)
+	# pprint.pprint(bbox_lef.blocks)
+	# pprint.pprint(bbox_lef.blocks[test2_mod.name].blocks[''].type)
+	# bbox_lef.build_lef()
+	# print(bbox_lef.lef)
+	# bbox_lef.write_lef()
